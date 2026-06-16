2b40ea6957b4bfa8d8f7c6c1fca7158cfa6ed955
QA-Priyank <priyanksrivastava40@gmail.com>
2026-06-16 18:28:18 +0530

    hetttt

---
+++ Priyank/test16june.js
@@ -0,0 +1,28 @@
+//re-declare --------- re-assign
+
+
+//let - re-assign ho sakta hai but re declare nahi kar sklate
+
+let name = 'Swati'
+name = 'SWATI S'
+
+console.log(name)
+console.log('***********************************************************')
+
+//var - re declare bhi kar skate hai and re assign bhi kar sakte hai
+
+var address = 'Noida'
+address = 'Raebareli'
+
+var address = 'lucknow'
+
+console.log(address)
+
+console.log('***********************************************************')
+
+//const - ye re-declare and reassign dono hi nahi hoga
+
+
+const age = 100
+console.log(age)
+
