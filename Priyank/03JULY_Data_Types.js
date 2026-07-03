31ae59f16870f3b8607381017a37ee8dec855946
QA-Priyank <priyanksrivastava40@gmail.com>
2026-07-03 21:06:56 +0530

    jj

---
+++ Priyank/03JULY_Data_Types.js
@@ -0,0 +1,62 @@
+// Variable declaration keywords
+// VAR ----- re declare and re assign
+// let ----- re assign but not re declare
+// const ---  neither re assign nor re declare
+
+
+
+
+// Data Types in JavaScript
+
+// Primitives Data types and Non-Primitives Data types
+// Primitive -- Mutable 
+// String, Number, Boolean, NULL, Undefind
+
+//string - '', ""
+let name = 'Swati'
+// for Print statement ---- console.log()
+
+console.log(name)
+console.log('***************************')
+
+
+//Number - 12, 0, 1.4
+ let age = 100
+
+ console.log(age)
+ console.log('***************************')
+
+//Boolean - True/False
+let PlaceNoida = false
+
+console.log(PlaceNoida)
+
+//NULL 
+let place = null
+console.log(place)
+
+
+//To run any script --------------
+//right click on file left side -- open integrated terminal 
+//node .\{file name}
+
+
+// Non-Primitives
+//  Array,Object, Function
+
+//Array 
+// VAR declaration Variable_name = []
+// array - when we store multiple elements data in a single array
+//let Details = [90,1.4,'Swati',true, false, 'huppp']
+
+//console.log(Details)
+
+
+//Indexing in Array
+// -- Postioning
+// strt with 0
+//             0   1    2     3     4         5
+let Details = [90,1.4,'Swati',true, false, 'huppp']
+
+console.log(Details[2])
+
