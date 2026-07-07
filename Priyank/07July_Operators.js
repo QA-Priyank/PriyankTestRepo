39df39e0dd19f55b147d1ecbcdd9efc58dafe1a3
QA-Priyank <priyanksrivastava40@gmail.com>
2026-07-07 16:33:12 +0530

    New

--- Priyank/07July_Operators.js
+++ Priyank/07July_Operators.js
@@ -1,2 +1,30 @@
 //Operators
 
+//Operators -------------------
+//Types  -----
+// Arithmetic opt -- + - % / *, **
+// Relational opt -- > < >= <= == != //=== !==
+
+// console.log(num1+num2) // Addition
+// console.log(num1-num2) // Subtract
+// console.log(num1/num2) // quotient/Divide
+// console.log(num1%num2) // reminder
+// console.log(num1*num2) // Multiplication
+// console.log(num1**num2) // square
+
+
+const num1 = 100
+const num2 = 100
+
+// console.log(num1>num2) // Greater than
+
+// console.log(num1<num2) // Less than
+
+//greater than equal to and less than equal to
+
+// console.log(num1>=num2) // Greater than
+
+// console.log(num1<=num2) // Less than
+
+
+
