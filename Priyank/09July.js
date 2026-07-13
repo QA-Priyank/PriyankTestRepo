7237d0168fe04cd137c226966035106cae891e77
QA-Priyank <priyanksrivastava40@gmail.com>
2026-07-13 16:29:18 +0530

    New

---
+++ Priyank/09July.js
@@ -0,0 +1,109 @@
+// How to find total count in Indexing (sub elements)
+//                    0                          1
+// //                0       1   2      3       0     1     2
+// const name = [['Priyank',60,'ABCD','PQR'],['ABC','XYZ','DEFGH']]
+
+// // console.log(name[0].length)
+
+
+
+
+
+
+
+
+
+
+
+
+// // // 10000 - me School aata hai -- how we know ki School me h aa rha h ki nhi?
+
+// let sent = 'My school is a wonderful place where I learn many new things every day. It has a big playground, a library, and well-equipped classrooms. My teachers are kind, helpful, and always encourage us to do our best. I enjoy studying different subjects, especially science and English'.split(' ')
+// // //Change in array (split('')-- kitte space dene hote hai)
+// // //use index of to find to word
+
+
+// let mango = sent[1].split('')
+// console.log(mango.includes('h'))
+// // for (let i=0; i<=mango.length-1; i++) {
+
+// //     if (mango[i]=='h'){
+// //         console.log('Test passed')
+// // }
+// // else {
+// //     console.log('Failed')
+// // }
+// // }
+
+
+
+// // // How to change Variable from string to numerical and vice versa
+
+
+// let num= '90'
+
+// console.log(typeof num)
+
+// let num1=Number(num)
+
+// console.log(typeof num1)
+
+// console.log(num, num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// How to find total count in Indexing (sub elements)
+
+//                        0                                     1
+//               0       1        2        3        0 1 -------
+// const name = [['Swati','Priyank','ABCD','dhhsjs'],[78,90,222,4,3,,2,42,3,23,2,322]]
+// // '6382823283294293029840294'
+// console.log(name[0].length)
+
+
+
+// 10000 - me School aata hai -- how we know ki School me h aa rha h ki nhi?
+
+// let name = 'My school is a wonderful place where I learn many new hello hi newthings every day. It has a big playground, a library, Swati and well-equipped classrooms. My teachers are kind, helpful, and always encourage us to do our best. I enjoy studying different subjects, especially science and English'.split(' ')
+// // string ---- array me convert
+// //console.log(name.includes('Swati'))
+// // Swati aata hi ki nhi --- includes ---- Check karta h ki data usme h ya nhi true/false
+// // Swati word  ki Indexing kya hai
+// //console.log(name.indexOf('Swati')) //21
+// console.log(name[21].includes('w'))
+
+
+ // How to change Variable from string to numerical and vice versa
+// PARSING 
+
+//  let num1 = '50' // variable change nahi hoga but iski value change kr sakti ho doosra variable create kr k
+//  console.log(typeof num1) //type of --- data type 
+
+//  const num2=Number(num1) 
+
+// console.log(typeof num2)
+
+// console.log(num1, num2)
+
+
+let num1 = 4033333333 
+
+console.log(typeof num1) //type of --- data type 
+const num2=String(num1) 
+
+console.log(typeof num2)
+console.log(num1, num2)
+
+
+
+
---
+++ Priyank/13July.js
@@ -0,0 +1,82 @@
+// Sync and Async -- Execution Method
+// Synchrionization & ASynchrionization 
+
+/*
+// Sync
+
+Test 1
+2
+3
+4
+
+// Async
+
+
+Test 1 VSC ==await 
+2 await 
+3
+4
+
+TC 5 TW
+
+async ---- sync //Change
+// await --- Async k andr aata h // by default time - 30 sec
+
+
+*/
+
+// if Else topic
+
+// Vote  age >(greater than) 18
+
+// if else --- Syntax 
+/*
+if (condition) {
+Print statement if condition is True
+}
+else {
+   Print staement if condition is False 
+    }
+*/
+
+// const age = 19
+
+// // if(age>=18){
+// //     console.log('Eligible for Vote')
+// // } else {
+// //     console.log('Not eligible')
+// // }
+
+// // ifelif statement -- Multiple condition k liye
+
+// if(age ==20 ) {
+//     console.log('Equal')
+// } else if (age>21){
+//     console.log('Greater')
+// }else {
+//     console.log('Less than')
+// }
+
+
+
+let browser = 'Firefox'
+
+if (browser == 'Chrome'){
+    console.log('browser should be open')
+} else if (browser == 'CHROME'){
+    console.log('browser  open')
+} else if (browser == 'chrome'){
+    console.log('browser correct')
+} else {
+    console.log('Browser is Invalid')
+}
+
+
+
+
+
+
+
+
+
+
