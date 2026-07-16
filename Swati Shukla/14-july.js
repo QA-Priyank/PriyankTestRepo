36d128939303ec65ac728c97ee4156a5ab56cbb4
Swati Shukla <Swati.Shukla@iboss.local>
2026-07-16 17:31:38 +0530

    new

---
+++ Swati Shukla/14-july.js
@@ -0,0 +1,41 @@
+// const username = 'swati1'
+
+// // if(username == 'swati123'){
+// //     console.log('allow go to password field')
+// // }
+// // else{
+// //     console.log('do not allow')
+// // }
+
+// if (username == 'swati'){
+//     console.log('should process')
+// }
+// else if (username == 123){
+//     console.log('should process int')
+// }
+// else {
+//     console.log('should not allow')
+// }
+
+let place = 'Noida' 
+let state = 'Uttar Pradesh'
+
+
+try {
+    if (place == 'delhi'){
+        console.log('place is incorrect')
+    }
+    else{
+        console.log('place correct')
+    }
+
+}
+catch(error){
+    console.log('place is not mine'+ error)
+}
+
+
+
+
+
+
